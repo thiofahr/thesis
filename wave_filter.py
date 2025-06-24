c46a5c307fdcef48f46b6a4a8fc21ea0cc92f2a5
Thio <82526650+thiofahr@users.noreply.github.com>
2025-06-24 10:20:04 +0700

    Update and rename wave_filtering.py to wave_filter.py

--- wave_filter.py
+++ wave_filter.py
@@ -1,26 +1,23 @@
-# Equatorial Wave Filtering
-
-# --- Import libraries
 import time
 import sys
 import numpy as np
-from scipy import signal
 import xarray as xr
 import matplotlib.pyplot as plt
 import dask.array as da
 import netCDF4
+from scipy import signal
 
-# --- Create a class for wave filtering
+# Wave Filter Class
 class WaveFilter:
-    def __init__(self, path, var, coverage, wave_name, n, units):
-        self.path       = path
+    def __init__(self, ds, var, coverage, wave_name, n, units):
+        #self.path       = path
         self.var        = var
         self.coverage  = coverage
         self.wave_name  = wave_name
         self.long_name  = None
         self.n          = n          
 
-        self.ds         = xr.open_dataset(path,chunks={'time': 'auto'})  # 使用 dask 分块加载
+        self.ds         = ds #xr.open_dataset(path,chunks={'time': 'auto'})  # 使用 dask 分块加载
         self.data       = None
         self.units      = units
 
@@ -77,11 +74,14 @@ class WaveFilter:
         if self.wave_name.lower() == "kw":
             self.tMin, self.tMax = 20, 180
             self.kmin, self.kmax = 1, 14
-            self.hmin, self.hmax = None, None #0.025, 90
+            self.hmin, self.hmax = 0.025, 50#0.025, 90
+            
         elif self.wave_name.lower() == "er":
-            self.tMin, self.tMax = 120, 450
+            self.tMin, self.tMax = 180, 450
             self.kmin, self.kmax = -10, -1
-            self.hmin, self.hmax = None, None #0.003, 90
+            self.hmin, self.hmax = 0.025, 50 #0.003, 90
+            
+        print(f"T: {self.tMin}-{self.tMax} days | k: {self.kmin}-{self.kmax} | h = {self.hmin}-{self.hmax} m")
         
         self.fmin, self.fmax = 1 / self.tMax, 1 / self.tMin
         self.mask =  da.zeros((self.data.shape[0], self.data.shape[2]), dtype=bool)
@@ -114,7 +114,6 @@ class WaveFilter:
         n    = self.n
 
         if self.wave_name.lower() == "kw":
-            
             if self.hmin is not None:
                 c      = da.sqrt(g * self.hmin)
                 omega  = 2. * np.pi * self.freq / 24. / 3600. / da.sqrt(beta * c)
@@ -126,8 +125,7 @@ class WaveFilter:
                 k      = self.knum / a * da.sqrt(c / beta)
                 self.mask = self.mask | (omega - k > 0)
     
-        if self.wave_name.lower() == "er":
-    
+        if self.wave_name.lower() == "er": 
             if self.hmin is not None:
                 c = da.sqrt(g * self.hmin)
                 omega = 2. * np.pi * self.freq / 24. / 3600. / da.sqrt(beta * c)
@@ -142,10 +140,10 @@ class WaveFilter:
     def inverse_fft(self):
         """Perform inverse FFT to get the filtered data."""
         self.filtered_data = da.fft.ifft2(self.fftdata, axes=(0, 2)).real
-
+    
     def create_output(self):
         """Create xarray DataArray for filtered data."""
-
+    
         if self.wave_name == 'KW':
             self.long_name = 'Kelvin Waves'
         elif 'ER' in self.wave_name:
@@ -172,16 +170,20 @@ class WaveFilter:
         
         self.ds.close()
         return self.wave_data
-        
-# --- Main program 
+
+
+# Main program 
 start=time.time()
+
 print("BEGIN")
 print(sys.version)
 
-# Data Information
-years = "2017-2024"
-var = "sla"
-path = f"/kaggle/input/sea-level-anomalies-noaa/sla.2017-2024.noaa.nc"
+# File paths
+file_address = {
+    "2023": ["/kaggle/input/ssh-copernicusmarine/ssh.2023-01-01_2023-12-31.nc", "/kaggle/input/ssh-copernicusmarine/ssh.2024-01-01_2024-12-31.nc"],
+    "2018": ["/kaggle/input/ssh-copernicusmarine/ssh.2018-01-01_2018-12-31.nc", "/kaggle/input/ssh-copernicusmarine/ssh.2019-01-01_2019-12-31.nc", "/kaggle/input/ssh-copernicusmarine/ssh.2020-01-01_2020-12-31.nc"],
+    "2014": ["/kaggle/input/ssh-copernicusmarine/ssh.2014-01-01_2014-12-31.nc", "/kaggle/input/ssh-copernicusmarine/ssh.2015-01-01_2015-12-31.nc", "/kaggle/input/ssh-copernicusmarine/ssh.2016-01-01_2016-12-31.nc"]
+}
 
 # Region of Interest
 coverage = {
@@ -189,21 +191,36 @@ coverage = {
     'lon' : slice(120, 280)
 }
 
-# Wave Types
-EquWaves = {
-    'KW'  : {'wave_name':'KW', 'n':0, 'data':[]}, 
-    'ER' : {'wave_name':'ER', 'n':1, 'data':[]}
+# Data Selection
+event = '2023'
+files = file_address[event]
+
+print(f"Year: {event}")
+
+# --- Load the Dataset ---
+# Raw Data
+ds = xr.open_mfdataset(files)
+var = 'zos'
+
+# Climatology Data
+clim = xr.open_dataset("/kaggle/input/ssh-copernicusmarine/cmems_mod_glo_phy_my_0.083deg_P1M-m_zos_120.00E-280.00E_10.00S-10.00N_1993-01-01-2021-06-01.nc", chunks="auto")
+
+# Anomaly Data
+anom = ds - clim.mean(dim='time')
+ds.close()
+
+equ_waves = {
+    'KW'  : {'wave_name':'KW', 'n':0, 'data':[]} 
+    'ER' : {'wave_name':'ER',  'n':1, 'data':[]}
 }
 
-# Loop through each wave type and apply the filter
-EWs = []
-for i, key in enumerate(EquWaves.keys()):
-    wave_name = EquWaves[key]['wave_name']
-    n         = EquWaves[key]['n']
+for i, key in enumerate(equ_waves.keys()):
+    wave_name = equ_waves[key]['wave_name']
+    n         = equ_waves[key]['n']
     print(f"{i+1}: {wave_name}, n = {n}")
 
     # create a wave filter object
-    wave_filter = WaveFilter(path, var, coverage, wave_name, n, units='m')
+    wave_filter = WaveFilter(anom, var, coverage, wave_name, n, units='m')
 
     # filtering process
     wave_filter.load_data()  
@@ -213,21 +230,21 @@ for i, key in enumerate(EquWaves.keys()):
     wave_filter.inverse_fft()
 
     # store the data
-    EquWaves[key]['data'] = wave_filter.create_output()
+    equ_waves[key]['data'] = wave_filter.create_output()
 
-    print(f"{i+1}/{len(EquWaves)} Completed")
+    print(f"{i+1}/{len(equ_waves)} Completed")
 
-# --- Create a dataset
+# CREATE DATASET
 # Extract the dimension
-t    = EquWaves['KW']['data'].time
-lat  = EquWaves['KW']['data'].lat
-lon  = EquWaves['KW']['data'].lon
+t    = equ_waves['ER']['data'].time
+lat  = equ_waves['ER']['data'].lat
+lon  = equ_waves['ER']['data'].lon
 
 # Initialize the dataset
 waves = xr.Dataset(
     data_vars={
-        key: (['time', 'lat', 'lon'], EquWaves[key]['data'].values, EquWaves[key]['data'].attrs)
-        for key in EquWaves if isinstance(EquWaves[key]['data'], xr.DataArray)
+        key: (['time', 'lat', 'lon'], equ_waves[key]['data'].values, equ_waves[key]['data'].attrs)
+        for key in equ_waves if isinstance(equ_waves[key]['data'], xr.DataArray)
     },
     coords=dict(
         time=t,
@@ -235,18 +252,21 @@ waves = xr.Dataset(
         lon=lon,
     ),
     attrs=dict(
-        description=f'Equatorial oceanic Kelvin and Rossby waves based on the sea level data of {years}',
+        description=f'Equatorial oceanic Kelvin and Rossby waves based on the sea level data of {event}',
+        source = 'Global Ocean Physics Reanalysis',
         unit='m'
     )
 )
 
+
 # Export the data
+print("Exporting the data...")
 comp = dict(zlib=True, complevel=5)
 encoding = {var: comp for var in waves.data_vars}
 waves.to_netcdf(
-    f'EW.SLA.{years}.noaa.nc',
-    engine='netcdf4',
-    encoding=encoding
+   f'EW.SLA.{event}.noaa.nc',
+   engine='netcdf4',
+   encoding=encoding
 )
 waves.close()
 
